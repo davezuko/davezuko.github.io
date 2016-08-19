306affcd4ef090c3d6f1bd65814b86631311a956
David Zukowski <zukoweb@gmail.com>
2016-08-19 16:27:06 -0400

    redeploy

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from './components/App.tsx'
-import './styles/main.sass'
-
-ReactDOM.render(<App />, window['root'])
