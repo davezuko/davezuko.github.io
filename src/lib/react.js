d79cbeb66a48d68fc081c9728131e8b43e2dcdde
David Zukowski <zukoweb@gmail.com>
2016-09-16 00:46:48 -0500

    chore(react): remove unused pseudo-hyperscript

--- src/lib/react.js
+++ src/lib/react.js
@@ -2,27 +2,50 @@ var React    = require('react')
   , ReactDOM = require('react-dom')
 
 var _slice = [].slice
-function last (xs) {
-  return xs[xs.length - 1]
-}
-function contains (c, a) {
-  return a.indexOf(c) !== -1
-}
-function withoutLast (xs) {
-  return xs.slice(0, xs.length - 1)
-}
+function contains (c, a) { return a.indexOf(c) !== -1 }
+function withoutLast (xs) { return _slice.call(xs, 0, dec(length(xs))) }
+function trim (s) { return s.trim() }
 
+var hyperToReact = (function () {
+  var splitTags = pipe(split('>'), map(trim))
+
+  // Noop atm
+  function mergeHyperProps (props, hsTag) { return props }
+
+  /**
+   * @param {String} tag - the tag name to parse from HyperScript into
+   * a tree of React elements.
+   * @param {[Object]} props - the props of the child element.
+   * @pram {[React.Element[]]} children - the children of the child element.
+   *
+   * @returns {React.Element}
+   */
+  return function hyperToReact (tag, props, children) {
+    var tags       = splitTags(tag)
+      , parents    = withoutLast(tags)
+      , childType  = last(tags)
+      , childProps = mergeHyperProps(props, childType)
+      , child
+
+    if (!children || length(children) <= 1) {
+      child = r(childType, childProps, first(children))
+    } else {
+      child = r.apply(null, concat([childType, childProps], children))
+    }
+    return foldr(function (acc, t) { return r(t, mergeHyperProps(null, t), acc)},
+                 parents)
+  }
+})()
+
+/**
+ * @param {String|Function} type - the type of element to create.
+ * @param {[Object]} props - the props to apply to the element.
+ *
+ * @returns {React.Element}
+ */
 function r (type, props, c0, c1, c2, c3, c4) {
-  var types, parents, child
-
-  if (typeof type === 'string' && contains('>', type)) {
-    types   = type.split('>').map(function (t) { return t.trim() })
-    parents = withoutLast(types)
-    child   = arguments.length <= 3
-      ? r(r(last(types)), props, c0)
-      : r.apply(null, [r(last(types)), props].concat(_slice.call(arguments, 2)))
-
-    return parents.reduceRight(function (acc, t) { return r(t, null, acc) }, child)
+  if (is('string', type) && contains('>', type)) {
+    return hyperToReact(type, props, _slice.call(arguments, 2))
   }
 
   switch (arguments.length) {
