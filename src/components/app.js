49eb440fb2a1d8a0cc4b622cf423d3b2b906b52a
David Zukowski <zukoweb@gmail.com>
2016-09-16 00:43:54 -0500

    chore(app): remove non-functional posts

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,7 @@ var posts = [
   { title: 'Eradicate Runtime Errors in React with Flow',
     href: 'http://technologyadvice.github.io/eradicate-runtime-errors-in-react-with-flow/' },
   { title: 'Where Flux Went Wrong',
-    href: 'http://technologyadvice.github.io/where-flux-went-wrong/' },
-  { title: 'New to JavaScript? Avoid Angular',
-    href: '/posts/new-to-js-avoid-angular.md' },
-  { title: 'Relay and GraphQL Introduction',
-    href: '/posts/relay-and-graphql-introduction.md' }]
+    href: 'http://technologyadvice.github.io/where-flux-went-wrong/' }]
 
 function Post (props) {
   return (
@@ -22,6 +18,6 @@ function App () {
       r('p', null, "Hey there, I'm a front-end developer currently obsessed with functional programming. I work professionally on large scale ReactJS applications, and in my free time enjoy contributing to the open source community and playing around with Clojure and Elm. Outside of work, I spend much of my time reading, playing tennis, and traveling."),
       r('p', null, "In the past I have worked on enterprise AngularJS applications, and before that developed websites for clients including Hilton Worldwide, Kimberly Clark, and Smucker's Natural Foods."),
       r('h2', null, 'Writing'),
-      r('ul', null, posts.map(Post))))}
+      r('ul', null, map(Post, posts))))}
 
 module.exports = App
