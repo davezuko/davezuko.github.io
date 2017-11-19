ce9d827f3ab35f959f16487200972b0c8adade7e
David Zukowski <zukoweb@gmail.com>
2017-11-19 11:38:20 -0600

    feat(writing): add published dates

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,14 +2,17 @@ const r = require('../../lib/react')
 const { map } = require('redash')
 
 const posts = [
-  { title: 'Where Flux Went Wrong', href: '/blog/where-flux-went-wrong' },
-  { title: 'Eradicate Runtime Errors in React with Flow', href: '/blog/eradicate-runtime-errors-in-react-with-flow' },
-  { title: 'From Redux to GraphQL', href: '/blog/from-redux-to-graphql' },
-  { title: 'New to JavaScript? Avoid Angular', href: '/blog/new-to-js-avoid-angular' }]
+  { title: 'Eradicate Runtime Errors in React with Flow', href: '/blog/eradicate-runtime-errors-in-react-with-flow', published: new Date(2016, 1, 28) },
+  { title: 'Where Flux Went Wrong', href: '/blog/where-flux-went-wrong', published: new Date(2015, 11, 30)},
+  { title: 'From Redux to GraphQL', href: '/blog/from-redux-to-graphql', published: new Date(2015, 8, 20) },
+  { title: 'New to JavaScript? Avoid Angular', href: '/blog/new-to-js-avoid-angular', published: new Date(2015, 5, 15) }]
 
 const projects = [
   { title: 'redash: Lightweight Functional Programming for JavaScript', source: 'https://github.com/davezuko/redash', href: 'https://redash.zuko.me' },
   { title: 'react-reformed: A Simpler Approach to Forms in React', source: 'https://github.com/davezuko/react-reformed', href: 'https://react-reformed.zuko.me' }]
+
+const formatDate = date =>
+  `${date.getFullYear()}`
 
 const HomeView = () =>
   r('main', null,
@@ -20,9 +23,11 @@ const HomeView = () =>
       'In the past I have worked on enterprise AngularJS applications, and before that developed websites for clients including Hilton Worldwide, Kimberly Clark, and Smucker\'s Natural Foods.'),
     r('h2', null, 'Writing'),
     r('ul', null,
-      map(({ title, href }) =>
+      map(({ title, href, published }) =>
         r('li', { key: title },
-          r('a', { href: href }, title)), posts)),
+          r('a', { href: href },
+            r('time', null, r('small', null, `(${formatDate(published)})`)),
+            ' ' + title)), posts)),
     r('h2', null, 'Projects'),
     r('ul', null,
       map(({ title, href }) =>
