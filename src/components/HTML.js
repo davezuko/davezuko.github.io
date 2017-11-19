5c284675230a6d7ba6531c6cd914bad82995c07b
David Zukowski <zukoweb@gmail.com>
2017-11-19 11:46:40 -0600

    fix(HTML): fix silent meta tag rendering error

--- src/components/HTML.js
+++ src/components/HTML.js
@@ -2,14 +2,14 @@ const r = require('../lib/react')
 const { mapi } = require('redash')
 
 const META = new Map([
-  ['viewport', 'width=device-width,initial-scale=1'],
+  ['viewport', 'width=device-width, initial-scale=1, shrink-to-fit=no'],
   ['google-site-verification', 'hvYlF72G6vlrz-XLjdsyLaEhqsmPDRYy2qR96KHmXhY'],
 ])
 
 const Head = ({ styles }) =>
   r('head', null,
     r('title', null, 'David Zukowski | Software Engineer'),
-    mapi(([name, content], i) => r('meta', { key: i, name, content }), META),
+    mapi(([name, content], i) => r('meta', { key: i, name, content }), Array.from(META)),
     mapi(({ href, body }, i) =>
       body ? r('style', { key: i, dangerouslySetInnerHTML: { __html: body } })
            : r('link', { key: i, rel: 'stylesheet', href }), styles))
