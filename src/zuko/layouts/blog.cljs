e30aaf8415666ae185fd2b4ee0302c17e39885e7
David Zukowski <david@zuko.me>
2018-06-16 21:59:30 -0700

    Refactor: simplify project tooling

--- src/zuko/layouts/blog.cljs
+++
@@ -1,7 +0,0 @@
-(ns zuko.layouts.blog
-  (:require [zuko.layouts.main :as default]
-            [zuko.utils :refer [html]]))
-
-(defn render [title content]
-  (default/render
-    [[:h1 title] content]))
